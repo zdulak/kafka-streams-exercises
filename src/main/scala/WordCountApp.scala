a62ed5b9042c485c37b9d8acdb9023b79617887c
Jacek Laskowski <jacek@japila.pl>
2022-05-04 09:54:34 +0200

    Tiny changes

--- src/main/scala/WordCountApp.scala
+++ src/main/scala/WordCountApp.scala
@@ -1,32 +1,30 @@
-import java.time.Duration
-
 object WordCountApp extends App {
+
   import org.apache.kafka.streams.StreamsConfig
+
   import java.util.Properties
 
   val props = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-pipe")
+  val appId = this.getClass.getSimpleName.replace("$", "")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, appId)
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 0)
-//  props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
-//  props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
 
   import org.apache.kafka.streams.KafkaStreams
-  import org.apache.kafka.streams.scala._
-  import org.apache.kafka.streams.scala.kstream._
   import org.apache.kafka.streams.scala.ImplicitConversions._
-  import org.apache.kafka.streams.kstream.Materialized
+  import org.apache.kafka.streams.scala._
   import org.apache.kafka.streams.scala.serialization.Serdes._
 
   val builder = new StreamsBuilder()
-  val textLines = builder.stream[String, String]("text-topic")
-  val wordCounts = textLines
+  builder.stream[String, String]("text-topic")
     .flatMapValues(textLine => textLine.split("\\W+"))
-    .groupBy((key, word) => word)
-    .count
+    .groupBy((_, word) => word)
+    .count()
     .mapValues(_.toString)
-  wordCounts.toStream.to("count-topic")
+    .toStream
+    .to("count-topic")
+  private val topology = builder.build()
 
-  val stream = new KafkaStreams(builder.build(), props)
+  val stream = new KafkaStreams(topology, props)
   stream.start()
 }
